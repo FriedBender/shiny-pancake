14998106ee4e489766644b8b6a5e50766404b7d2
Maksim Semchuk <maxsemchuk@icloud.com>
2021-05-14 11:25:05 -0700

    Made sure objects are read in

--- vegify/read_csv.py
+++ vegify/read_csv.py
@@ -5,13 +5,20 @@ import os
 
 
 def open_read_csv_file(file_to_read_in):
-    with open(file_to_read_in, newline='') as csvfile:
-        opened_file = csv.reader(csvfile, delimiter=' ' , quotechar='|')
-        for row in opened_file:
-            print(', '.join(row))
-    return opened_file
+    file_obj = open(file_to_read_in, 'r', newline='')
+    return csv.reader(file_obj)
 
 
 if __name__ == "__main__":
-    vegify_csv_file = 'GoVegifyMenuAnalyzerData-menuItemTitleReps.csv'
-    open_read_csv_file(vegify_csv_file)
+    location_of_skus = '/home/maksim/Documents/School/CS 469 - Capstone I/playground/Vegify/itemSKUs.csv'
+    location_of_Menu_Alternatives = '/home/maksim/Documents/School/CS 469 - Capstone I/playground/Vegify/MenuItemAlternatives.csv'
+    location_of_Menu_Item_Ingrediants = '/home/maksim/Documents/School/CS 469 - Capstone I/playground/Vegify/menuItemIngrediants.csv'
+    
+    Skus_Csv = open_read_csv_file(location_of_skus)
+    Alternatives_Csv = open_read_csv_file(location_of_Menu_Alternatives)
+    Menu_Ingrediants = open_read_csv_file(location_of_Menu_Item_Ingrediants)
+
+    print(f'\nSku CSV File:\n{Skus_Csv}\nIngredients CSV File:\n{Alternatives_Csv}\nMenu Ingrediants CSV File:\n{Menu_Ingrediants}\n')
+
+    for line in Skus_Csv:
+        print(line)
