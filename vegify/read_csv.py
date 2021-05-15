96b2ec11122a42e6359f07c6d3620ca476266780
Maksim Semchuk <maxsemchuk@icloud.com>
2021-05-15 09:28:41 -0700

    all csv reading works

--- vegify/read_csv.py
+++ vegify/read_csv.py
@@ -1,12 +1,16 @@
-# File to read in CSV
-
 import csv
 import os
 
 
 def open_read_csv_file(file_to_read_in):
+    # This function opens a file that is passed in determines the dialect (what format CSV file it is)
+    # Then opens the file, and resets the carriage return to top of file.
+    # Then returns the file
     file_obj = open(file_to_read_in, 'r', newline='')
-    return csv.reader(file_obj)
+    dialect = csv.Sniffer().sniff(file_obj.read())
+    file_obj.seek(0)
+    print(dialect)
+    return csv.reader(file_obj, dialect)
 
 
 if __name__ == "__main__":
@@ -18,7 +22,4 @@ if __name__ == "__main__":
     Alternatives_Csv = open_read_csv_file(location_of_Menu_Alternatives)
     Menu_Ingrediants = open_read_csv_file(location_of_Menu_Item_Ingrediants)
 
-    print(f'\nSku CSV File:\n{Skus_Csv}\nIngredients CSV File:\n{Alternatives_Csv}\nMenu Ingrediants CSV File:\n{Menu_Ingrediants}\n')
-
-    for line in Skus_Csv:
-        print(line)
+    print(f'\n\nSKU CSV File:\n{list(Skus_Csv)}\nIngredients CSV File:\n\n{list(Alternatives_Csv)}\nMenu Ingrediants CSV File:\n\n{list(Menu_Ingrediants)}\n')
--- vegify/read_csv_test.py
+++ vegify/read_csv_test.py
@@ -1,6 +1,7 @@
-import unittest
+import pytest
 from . import read_csv
 import csv
+import pandas
 
 # These are local to FriedBenders file structure, modify them as needed
 location_of_skus = '/home/maksim/Documents/School/CS 469 - Capstone I/playground/Vegify/itemSKUs.csv'
@@ -9,7 +10,6 @@ location_of_Menu_Item_Ingrediants = '/home/maksim/Documents/School/CS 469 - Caps
 
 class TestRead_Csv(unittest.TestCase):
 
-    def test_open_read_csv_file(self):
-        csv_file = '/home/maksim/Documents/School/CS 469 - Capstone I/playground/Vegify/GoVegifyMenuAnalyzerData-menuItemTitleReps.csv'
-        result = read_csv.open_read_csv_file(csv_file)
+    def test_open_read_csv_file() -> None:
+        assert read_csv.open_read_csv_file(location_of_skus) == file
         self.assertIsInstance(result, csv.Dialect(csv_file))
