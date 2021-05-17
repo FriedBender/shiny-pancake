5cd11c79b1169e8161c6ef56ed6c91d760913ca7
Maksim Semchuk <maxsemchuk@icloud.com>
2021-05-16 22:09:46 -0700

    added some additional notes and fixed a function

--- vegify/read_csv.py
+++ vegify/read_csv.py
@@ -3,7 +3,7 @@ import os
 
 # TODO: parse in such a way:
 # 0. Add another column that holds the price of the unit (as a whole) in itemSKUs.csv - Done
-# 1. dict: non vegan option maps to a vegan option, but with 1 key, multiple values (menuItemIngrediants.csv)
+# 1. dict: non vegan option maps to a vegan option, but with 1 key, multiple values (menuItemIngrediants.csv) - Done
 # 2. dict: create another dictionary that reads in vegan options as keys, and values are price.
 
 
@@ -29,30 +29,47 @@ def create_dictionary_object(csv_object) -> dict:
         dictionary_object[key] = row[1:]
     return dictionary_object
 
-def print_dict(header, dictionary_object: dict):
+
+def print_dict(header, dictionary_object: dict) -> None:
     # Theoretically, this function is going to print out a pretty version of the
     # dicationary object, which will be formatted in some way....... hopefully.
     print("\n")
-    print(f'{header}\n')
-    print(dictionary_object)
+    print(f'{header}')
+    for key, value in dictionary_object.items():
+        print(f'Key: {key} --- Value: {value}')
     print("\n")
+
 
 
 
 if __name__ == "__main__":
+
+    # Max's computer locations
     location_of_skus = '/home/maksim/Documents/School/CS 469 - Capstone I/playground/Vegify/itemSKUs.csv'
     location_of_Menu_Alternatives = '/home/maksim/Documents/School/CS 469 - Capstone I/playground/Vegify/MenuItemAlternatives.csv'
     location_of_Menu_Item_Ingrediants = '/home/maksim/Documents/School/CS 469 - Capstone I/playground/Vegify/menuItemIngrediants.csv'
     
+    # Macbook locations
     mac_location_of_skus = '/Users/max/Desktop/shiny-pancake/itemSKUs.csv'
     mac_location_of_Menu_Alternatives = '/Users/max/Desktop/shiny-pancake/MenuItemAlternatives.csv'
     mac_location_of_Menu_Item_Ingrediants = '/Users/max/Desktop/shiny-pancake/menuItemIngrediants.csv'
 
+    # Create the csv objects
+    skus_header, skus_csv = open_read_as_dict_csv_file(mac_location_of_skus)
+    vegan_alternatives_header, vegan_alternatives_csv = open_read_as_dict_csv_file(mac_location_of_Menu_Alternatives)
+    menu_Ingrediants_header, menu_Ingrediants_csv = open_read_as_dict_csv_file(mac_location_of_Menu_Item_Ingrediants)
 
-    Skus_csv = open_read_as_dict_csv_file(mac_location_of_skus)
-    Alternatives_csv = open_read_as_dict_csv_file(mac_location_of_Menu_Alternatives)
-    csv_menu_Ingrediants_header, Menu_Ingrediants_csv = open_read_as_dict_csv_file(mac_location_of_Menu_Item_Ingrediants)
+    # create dictionary values
+    skus_dictionary_structure = create_dictionary_object(skus_csv)
+    vegan_alternatives_dictionary_structure = create_dictionary_object(vegan_alternatives_csv)
+    menu_Ingrediants_dictionary_structure = create_dictionary_object(menu_Ingrediants_csv)
+    
+    # Print all dictionary objects
+    print_dict(skus_header, skus_dictionary_structure)
+    print_dict(vegan_alternatives_header, vegan_alternatives_dictionary_structure)
+    print_dict(menu_Ingrediants_header, menu_Ingrediants_dictionary_structure)
 
-    #   print_dict(csv_menu_Ingrediants_header, Menu_Ingrediants_csv)
-    Menu_Ingrediants_dictionary = create_dictionary_object(Menu_Ingrediants_csv)
-    print(Menu_Ingrediants_dictionary)
+    # example to get the price of Beyond Meat Burger Patty
+    item = skus_dictionary_structure.get('Beyond Meat Burger Patty')
+    # How to access the price of the sku object
+    print(f'\n\n\n\nitem: {item[3]}\n\n')
